c1623a1b5f94acdf8db9b61b44ecdbd660ab4486
AssafUni <46829706+AssafUni@users.noreply.github.com>
2021-07-25 03:58:19 +0300

    Update turtlebot3_dqn.py

--- src/turtlebot3_dqn.py
+++ src/turtlebot3_dqn.py
@@ -271,7 +271,7 @@ class ReinforceAgent():
         goal_vel = tf.convert_to_tensor(goal_vel, dtype=tf.float64)
         action = tf.convert_to_tensor(action, dtype=tf.int64)
         reward = tf.convert_to_tensor(reward, dtype=tf.float64)
-        next_costmap = tf.convert_to_tensor(reward, dtype=tf.int64)
+        next_costmap = tf.convert_to_tensor(next_costmap, dtype=tf.int64)
         next_goal_vel = tf.convert_to_tensor(next_goal_vel, dtype=tf.float64)
         done = tf.convert_to_tensor(done, dtype=tf.bool)
         self.memory.append((costmap, goal_vel, action, reward, next_costmap, next_goal_vel, done))
